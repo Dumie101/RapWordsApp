4c57640947b26fc7a07e4b64c514c24157e6b037
banskota <ic905014@dal.ca>
2022-01-20 16:46:31 -0400

    Commit 7: tryna save state

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/main.dart';
 import 'package:flutterapp/homepage.dart';
@@ -17,36 +18,15 @@ class MyMainPage extends StatefulWidget {
 class _MyMainPageState extends State<MyMainPage> {
   List<RhymeWord>? words;
 
-  _MyMainPageState({required this.words}) {}
+  _MyMainPageState({required this.words});
 
   @override
   Widget build(BuildContext context) {
-    final _textController = TextEditingController();
-
     return MaterialApp(home: Builder(builder: (context) {
       return Scaffold(
         body: Column(
-          children: [
-            Container(
-              child: Expanded(
-                  child: ListView.builder(
-                      itemCount: null == words ? 0 : words!.length,
-                      itemBuilder: (context, index) {
-                        RhymeWord rhymeWord = words![index];
-                        return Card(
-                          child: ListTile(
-                            title: Text(
-                              rhymeWord.word,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        );
-                      })),
-            )
-          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [NewWidget(words: words)],
         ),
         bottomNavigationBar: BottomNavigationBar(
           showSelectedLabels: false,
@@ -64,8 +44,10 @@ class _MyMainPageState extends State<MyMainPage> {
             BottomNavigationBarItem(
                 icon: IconButton(
                     onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => SearchPage()));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SearchPage()));
                     },
                     icon: const Icon(Icons.search)),
                 label: 'Search'),
@@ -81,5 +63,60 @@ class _MyMainPageState extends State<MyMainPage> {
         ),
       );
     }));
+  }
+}
+
+class NewWidget2 extends StatelessWidget {
+  const NewWidget2({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: DefaultTextStyle(
+          style: const TextStyle(
+              color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold),
+          child: AnimatedTextKit(
+            repeatForever: true,
+            animatedTexts: [
+              TyperAnimatedText('No Rhyme Words Found.'),
+              TyperAnimatedText('Search Again.')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    Key? key,
+    required this.words,
+  }) : super(key: key);
+
+  final List<RhymeWord>? words;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: ListView.builder(
+            itemCount: null == words ? 0 : words!.length,
+            itemBuilder: (context, index) {
+              RhymeWord rhymeWord = words![index];
+              return Card(
+                child: ListTile(
+                  title: Text(
+                    rhymeWord.word,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              );
+            }));
   }
 }
